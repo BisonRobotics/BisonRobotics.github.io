02b1de66fe59de2876d84509abdc44f3ad29acda
brandongasser <bgasser12@gmail.com>
2024-03-22 15:22:35 -0500

    Add sponsors content

--- src/app/sponsors/sponsors.component.ts
+++ src/app/sponsors/sponsors.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sponsors',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './sponsors.component.html',
   styleUrl: './sponsors.component.css'
 })
 export class SponsorsComponent {
+
+  collegiateSponsors: { name: string, imgSrc: string }[] = [
+    { name: 'NDSU Mechanical Engineering', imgSrc: '' },
+    { name: 'NDSU Student Government', imgSrc: '' },
+    { name: 'NDSU Industrial Manufacturing Engineering', imgSrc: '' },
+    { name: 'NDSU College of Engineering', imgSrc: '' }
+  ];
+
+  levelFiveSponsors: { name: string, imgSrc: string }[] = [];
+
+  levelFourSponsors: { name: string, imgSrc: string }[] = [
+    { name: 'Marvin', imgSrc: '' },
+    { name: 'Blow Molded Specialties', imgSrc: '' },
+    { name: 'Packet Digital', imgSrc: '' }
+  ];
+
+  levelThreeSponsors: { name: string, imgSrc: string }[] = [
+    { name: 'Dakota Lithium', imgSrc: '' }
+  ];
+
+  levelTwoSponsors: { name: string, imgSrc: string }[] = [
+    { name: 'American Engineering Testing', imgSrc: '' },
+    { name: 'Bobcat', imgSrc: '' },
+    { name: 'Lulzbot', imgSrc: '' }
+  ];
+
+  levelOneSponsors: { name: string, imgSrc: string }[] = [
+    { name: 'Apec Engineering Group', imgSrc: '' }
+  ];
 
 }
